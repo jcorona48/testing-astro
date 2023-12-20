944da74aebce3fdb63900bf6d2d21a02926a8f6d
Joan Corona <joaneliasc86@gmail.com>
2023-12-20 00:07:27 -0400

    quit netilfy

--- .netlify/functions-internal/ssr/chunks/generic_3Jn9lvGY.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_eiv2v0SR.mjs');
-
-export { page };
