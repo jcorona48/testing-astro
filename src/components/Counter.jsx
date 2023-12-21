f9237a37d54b6811120f19ea9628b6a49b4ead54
Joan Corona <joaneliasc86@gmail.com>
2023-12-21 18:11:25 -0400

    mod counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,30 +1,20 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { toast } from "react-toastify";
 const Counter = () => {
     const [count, setCount] = useState(0);
+
     const [isClicked, setIsClicked] = useState(false);
+
     const handleCount = () => {
         setCount(count + 1);
-        toast.promise(
-            () => {
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        reject("Counter increased");
-                    }, 1000);
-                });
-            },
-            {
-                pending: "Increasing counter",
-                success: "Counter increased",
-                error: "Cannot increase counter",
-            }
-        );
+        toast.success("Counter is clicked");
         setIsClicked(true);
         setTimeout(() => {
             setIsClicked(false);
         }, 300);
     };
+
     return (
         <button
             className={`p-2 bg-blue-500 text-white rounded w-32 hover:bg-blue-600 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 active:bg-blue-700 active:ring-2 active:ring-blue-400 active:ring-opacity-50 ${
